ea1eec51390f1600148647d617beca9fedb6197b
Henry <91667944+5thAttemptCode@users.noreply.github.com>
2023-09-04 02:06:43 +0200

    Add files via upload

---
+++ Vue TresJs Basic Starter/src/App.vue
@@ -0,0 +1,31 @@
+<script setup>
+import { TresCanvas } from '@tresjs/core'
+import { OrbitControls, MeshGlassMaterial } from '@tresjs/cientos';
+</script>
+
+
+<template>
+ <TresCanvas clear-color="white" window-size>
+    <TresPerspectiveCamera :position="[3, 1, 3]" :look-at="[0, 0, 0]" />
+    <OrbitControls />
+    <TresMesh>
+      <TresBoxGeometry :args="[1, 1, 1]" />
+      <MeshGlassMaterial color="blue"/>
+    </TresMesh>
+  </TresCanvas>
+</template>
+
+
+<style scoped>
+
+  *{
+    margin: 0;
+    padding: 0;
+  }
+
+  body, #app  {
+    height: 100vh;
+    width: 100%;
+  }
+
+</style>
